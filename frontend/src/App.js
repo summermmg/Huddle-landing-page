2bb3d3c6cf5e3dc41f0a6d07a543c46ff739690f
“Summer” <summerzoummg@gmail.com>
2022-01-05 22:24:18 -0500

    create app

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import { Container } from './components/styles/Container.styled'
+function App() {
+  return (
+    <Container>
+      <h1>Styled Component</h1>
+    </Container>
+  );
+}
+
+export default App;
