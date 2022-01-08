4807fc62c81be185703db9db7239b4e352930177
“Summer” <summerzoummg@gmail.com>
2022-01-08 00:30:25 -0500

    add styled card component
    add styled card component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import { ThemeProvider } from 'styled-components'
 import { Container } from './components/styles/Container.styled'
 import Header from './components/Header' 
 import GlobalStyles from './components/styles/Global'
+import content from './content'
+import Card from './components/Card'
 
 const theme = {
   colors: {
     header: '#ebfbff',
     body: '#fff',
     footer: '#003333'
-  }
+  },
+  mobile: '368px'
 }
 
 function App() {
@@ -17,7 +20,9 @@ function App() {
       <GlobalStyles />
       <Header />
       <Container>
-        <h1>Styled Component</h1>
+        {content.map((item) => (
+          <Card key={item.id} item={item}></Card>
+        ))}
       </Container>
     </ThemeProvider>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { StyledHeader, Nav, Logo } from './styles/Header.styled'
+import { StyledHeader, Nav, Logo, Image } from './styles/Header.styled'
 import { Container } from './styles/Container.styled'
 import { Button } from './styles/Button.styled'
+import { Flex } from './styles/Flex.styled'
 
 const Header = () => {
     return (
@@ -10,7 +11,20 @@ const Header = () => {
                 <Nav>
                     <Logo src="./images/logo.svg" alt="" />
                     <Button>Try It Free</Button>    
-                </Nav>          
+                </Nav>
+
+                <Flex>
+                    <div>
+                        <h1>
+                            Build The Community Your Fans Will Love
+                        </h1>
+                        <p>Huddle re-imagines the way we build communities. 
+                            You have a voice, but so does your audience. Create connections
+                             with your users as you engage in genuine discussion.</p>    
+                        <Button bg="hsl(322, 100%, 66%)" color="#fff">Get Started For Free</Button>     
+                    </div>
+                    <Image src="./images/illustration-mockups.svg"></Image>    
+                </Flex>          
             </Container>
         </StyledHeader>
     )
